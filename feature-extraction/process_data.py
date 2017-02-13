17aae1077b1a25c1a99a1407b41216e41ce1eee3
AxelGoetz <axel.goetz@hotmail.com>
2017-02-13 12:54:12 +0000

    Update the processing data model not to return a dict anymore but instead an array

--- feature-extraction/process_data.py
+++ feature-extraction/process_data.py
@@ -2,11 +2,12 @@
 Pulls all of the data in the ../data directory into memory
 """
 
-from os import scandir
+from os import scandir, path
 from sys import stdout, exit
 
 # CONSTANTS
-DATA_DIR = '../data/cells'
+dirname, _ = path.split(path.abspath(__file__))
+DATA_DIR = dirname + '/../data/cells'
 UNKNOWN_WEBPAGE = -1
 TRACE_DELIMITER = '\t'
 
@@ -14,14 +15,6 @@ def update_progess(total, current):
     """Prints a percentage of how far the process currently is"""
     stdout.write("{:.2f} %\r".format((current/total) * 100))
     stdout.flush()
-
-def add_key_to_dict(dictionary, key, init):
-    """
-    Given a dictionary, checks if the key already exists.
-    If not, it initializes the entry with an init value.
-    """
-    if key not in dictionary:
-        dictionary[key] = init
 
 def read_cell_file(f):
     """
@@ -48,11 +41,11 @@ def import_data(data_dir=DATA_DIR):
     Reads all of the files in the `data_dir` and returns all of the contents in a variable.
 
     @param data_dir is a string with the name of the data directory
-    @return data is a dictionary with the following format: {website_index: [size, incoming]}
+    @return data is an array with the following format: [([size, incoming], webpage_label)}
         where incoming is 1 is outgoing and -1 is incoming
     """
     stdout.write("Starting data import\n")
-    data = {}
+    data = []
 
     total_files = len([f for f in scandir(data_dir) if f.is_file()])
 
@@ -63,15 +56,14 @@ def import_data(data_dir=DATA_DIR):
 
             name_split = name.split('-') # Contains [webpage, index (OPTIONAL)]
 
-            key = None
+            webpage_label = None
             # If the length is 1, classify as unknown webpage
             if len(name_split) == 1:
-                key = UNKNOWN_WEBPAGE
+                webpage_label = UNKNOWN_WEBPAGE
             else:
-                key = int(name_split[0])
+                webpage_label = int(name_split[0])
 
-            add_key_to_dict(data, key, [])
-            data[key].append(read_cell_file(f))
+            data.append((read_cell_file(f), webpage_label))
 
             if i % 100 == 0:
                 update_progess(total_files, i)
