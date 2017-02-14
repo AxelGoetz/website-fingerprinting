4898ad98e32522b70a60ee8d02f86d203c4fdb3d
AxelGoetz <axel.goetz@hotmail.com>
2017-02-14 10:43:11 +0000

    Add helpers + tests

---
+++ feature_extraction/helpers.py
@@ -0,0 +1,82 @@
+"""
+Includes all of the helper functions such as:
+- Getting new batches of data
+- Converting data to time-major format
+"""
+
+import numpy as np
+
+def add_EOS(data, sequence_lengths, EOS=-1):
+    """
+    For the decoder targets, we want to add a EOS symbol after the end of each sequence
+
+    @param data in **batch-major** format
+    @param sequence_lengths is a array-like object with the sequence lengths of the elements in the data object
+    @param EOS is the end-of-sequence marker
+    """
+    for i, sequence in enumerate(data):
+        sequence[sequence_lengths[i]] = EOS
+
+
+def time_major(data):
+    """
+    Return the data in time-major form (Most often used to turn a single batch into this form)
+    *(Assumes the traces are already padded)*
+
+    @param data is an array of sequences
+    @return batch contains the batch in **time-major** form [max_time, batch_size] padded with the PAD symbol
+    """
+    # Swap axis
+    return data.swapaxes(0, 1)
+
+def shuffle_data(data, seed=123):
+    """
+    Shuffles an array-like object
+    """
+    np.random.seed(seed)
+    np.random.shuffle(data)
+
+
+def get_batches(data, batch_size=100):
+    """
+    Divides the data up into batches
+
+    @param data is an array of sequences
+    @param batch_size is the size of each batch
+
+    @return an iterator with the batch sizes
+    """
+    # TODO: Perhaps we can take the class distribution in each class into consideration
+
+    # Randomly shuffle the data
+    shuffle_data(data)
+
+    data_length = len(data)
+
+    for i in range(0, data_length, batch_size):
+        start_index = i
+        end_index = i + batch_size if i + batch_size < data_length else data_length
+
+        yield data[start_index: end_index]
+
+
+def pad_traces(data, extra_padding=5):
+    """
+    Pad the traces such that they have the same length
+
+    @param data is an 2D matrix in the following format: [[size, incoming]]
+    @param extra_padding determines how much extra padding is added to the elements
+    @return a tuple where the first element is the padded matrix and the second the lengths
+    """
+    sequence_lengths = [len(seq) for seq in data]
+    batch_size = len(data)
+
+    max_sequence_length = max(sequence_lengths) + extra_padding
+
+    inputs_batch_major = np.zeros(shape=[batch_size, max_sequence_length], dtype=np.int32)
+
+    for i, seq in enumerate(data):
+        for j, element in enumerate(seq[0]):
+            inputs_batch_major[i, j] = element
+
+    return inputs_batch_major, sequence_lengths
---
+++ tests/test_helpers.py
@@ -0,0 +1,53 @@
+import numpy as np
+import unittest
+
+from feature_extraction import helpers
+
+class HelperTests(unittest.TestCase):
+
+    def setUp(self):
+        self.data = np.array([[1, 2, 3, 4, 0], [1, 0, 0, 0, 0], [3, 4, 5, 0, 0], [40, 3, 2, 0, 0]])
+        self.sequence_lengths = np.array([4, 1, 3, 3])
+        self.labels = np.array([-1, 1, 2, 3])
+
+    def testEOS(self):
+        helpers.add_EOS(self.data, self.sequence_lengths)
+
+        self.assertTrue((self.data[0] == np.array([1, 2, 3, 4, -1])).all())
+        self.assertTrue((self.data[1] == np.array([1, -1, 0, 0, 0])).all())
+        self.assertTrue((self.data[2] == np.array([3, 4, 5, -1, 0])).all())
+        self.assertTrue((self.data[3] == np.array([40, 3, 2, -1, 0])).all())
+
+    def test_time_major(self):
+        new_data = helpers.time_major(self.data)
+
+        self.assertTrue((new_data[0] == np.array([1, 1, 3, 40])).all())
+        self.assertTrue((new_data[1] == np.array([2, 0, 4, 3])).all())
+        self.assertTrue((new_data[2] == np.array([3, 0, 5, 2])).all())
+        self.assertTrue((new_data[3] == np.array([4, 0, 0, 0])).all())
+        self.assertTrue((new_data[4] == np.array([0, 0, 0, 0])).all())
+
+    def test_shuffle_data(self):
+        data_batch = helpers.get_batches(self.data)
+        sequence_lengths_batch = helpers.get_batches(self.sequence_lengths)
+        labels_batch = helpers.get_batches(self.labels)
+
+        r_data = next(data_batch)
+        r_sequence_length = next(sequence_lengths_batch)
+        r_labels = next(labels_batch)
+
+        for i, val in enumerate(r_data):
+            if val[0] == 40:
+                self.assertEqual(r_sequence_length[i], 3)
+                self.assertEqual(r_labels[i], 3)
+
+    def test_pad_traces(self):
+        self.data = [[[1.1, -1], [2, 1], [3, 1], [4, 1]], [[1, -1]]]
+
+        padded_matrix, sequence_lengths = helpers.pad_traces(self.data)
+
+        self.assertEqual(len(padded_matrix[0]), 9)
+        self.assertEqual(sequence_lengths[0], 4)
+
+        self.assertEqual(len(padded_matrix[1]), 9)
+        self.assertEqual(sequence_lengths[1], 1)
