6593eb83c4e975e41a6ff16ae853f0a96baa6897
AxelGoetz <axel.goetz@hotmail.com>
2017-02-16 18:10:29 +0000

    Running model

--- feature_extraction/run_model.py
+++ feature_extraction/run_model.py
@@ -1,11 +1,34 @@
-from process_data import import_data
-# import model
-import temp
-
+import tensorflow as tf
+from tensorflow.contrib.rnn import LSTMCell, GRUCell
 import numpy as np
 
 from importlib import reload
 from sys import stdin, stdout
+
+from new_model import Seq2SeqModel, train_on_copy_task
+from process_data import import_data
+
+def run_model(data):
+    tf.reset_default_graph()
+    tf.set_random_seed(123)
+
+    with tf.Session() as session:
+
+        # with bidirectional encoder, decoder state size should be
+        # 2x encoder state size
+        model = Seq2SeqModel(encoder_cell=LSTMCell(20),
+                             decoder_cell=LSTMCell(20),
+                             max_sequence_length=len(data[0]),
+                             seq_width=2,
+                             batch_size=100)
+
+        session.run(tf.global_variables_initializer())
+
+        train_on_copy_task(session, model, data,
+                           batch_size=100,
+                           max_batches=3000,
+                           batches_in_epoch=100,
+                           verbose=True)
 
 cache_data, labels = None, None
 if __name__ == '__main__':
@@ -16,15 +39,14 @@ if __name__ == '__main__':
                 cache_data, labels  = import_data()
 
             stdout.write("Training on data...\n")
-            temp.temp(cache_data)
+            run_model(cache_data)
             stdout.write("Finished running model.")
 
             # Wait for enter
             stdin.readline()
 
             # Reload the source code
-            # reload(model)
-            reload(temp)
+            reload(new_model)
 
     except KeyboardInterrupt:
         stdout.write("Interrupted, this might take a while...\n")
