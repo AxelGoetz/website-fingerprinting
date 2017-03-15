8b079e1d2fb6839197d3419febcc59a9054b09c5
AxelGoetz <axel.goetz@hotmail.com>
2017-03-15 16:20:49 +0000

    Add option to extract features from X_train only

--- feature_extraction/feature_extraction.py
+++ feature_extraction/feature_extraction.py
@@ -29,12 +29,53 @@ def write_to_file(content, save_dir, file_name):
 
 def create_dir_if_not_exists(directory):
     """
-    Creates a dir if it does not exists
+    Creates a dir if it does not exists, if it exists, it deletes it and creates a new one
 
     @param directory is a string containing the **absolute** path to the directory
     """
-    if not ospath.exists(directory):
-        makedirs(directory)
+    if ospath.exists(directory):
+        from shutil import rmtree
+        rmtree(directory)
+
+    makedirs(directory)
+
+def extract_features_from_files(feature_extraction, paths, save_dir, extension=".cell", model_name=""):
+    """
+    Given a list of paths, extract the features from these paths and stores them in the appropriate directory and file
+
+    @param feature_extraction is a function that takes a trace as an input and returns a list of features (1D)
+    @param path is a list of paths to the raw cell files
+    @param save_dir is the directory where you save the features for the traces.
+        Every in this dir is called `{website}-{id}.cellf` with both `website` and `id` being integers
+    @param extension is the extension of the files that contain the raw traces
+    @param model_name is used for printing for what model we are extracting features
+    """
+    if model_name == "":
+        stdout.write("Starting to extract features\n")
+    else:
+        stdout.write("Starting to extract features for {}\n".format(model_name))
+
+    create_dir_if_not_exists(save_dir)
+    total_files = len(paths)
+
+    for i, path in enumerate(paths):
+        try:
+            file_content = read_cell_file(path)
+        except:
+            print("Cannot read file: " + path)
+            exit(0)
+
+        features = feature_extraction(file_content)
+
+        file_name = ospath.basename(path)
+        file_name = file_name.replace(extension, "")
+        file_name += ".cellf"
+
+        write_to_file(features, save_dir, file_name)
+
+        if i % 50 == 0:
+            update_progess(total_files, i)
+
 
 def extract_features(feature_extraction, save_dir, data_dir=DATA_DIR, extension=".cell", model_name=""):
     """
@@ -45,55 +86,75 @@ def extract_features(feature_extraction, save_dir, data_dir=DATA_DIR, extension=
         Every in this dir is called `{website}-{id}.cellf` with both `website` and `id` being integers
     @param data_dir is the absolute path to the data directory
     @param extension is the extension of the files that contain the raw traces
-    @param feature_name is used for printing for what model we are extracting features
+    @param model_name is used for printing for what model we are extracting features
     """
-    if model_name == "":
-        stdout.write("Starting to extract features\n")
-    else:
-        stdout.write("Starting to extract features for {}\n".format(model_name))
-
-    create_dir_if_not_exists(save_dir)
-
-    total_files = len([f for f in scandir(data_dir) if f.is_file()])
-
+    paths = []
     for i, f in enumerate(scandir(data_dir)):
         if f.is_file() and f.name[-len(extension):] == extension:
-            try:
-                file_content = read_cell_file(f.path)
-            except:
-                print(f.path)
-                exit(0)
-            features = feature_extraction(file_content)
+            paths.append(f.path)
 
-            file_name = f.name
-            file_name = file_name.replace(extension, "")
-            file_name += ".cellf"
-
-            write_to_file(features, save_dir, file_name)
-
-        if i % 50 == 0:
-            update_progess(total_files, i)
+    extract_features_from_files(feature_extraction, paths, save_dir, extension=extension, model_name=model_name)
 
 
-def extract_all_features(save_dir, data_dir=DATA_DIR):
+def extract_partial_features(file_path, save_dir, data_dir=DATA_DIR, extension=".cell"):
+    """
+    Only extracts the features from the paths in the `file_path` file
+    """
     from naive_bayes import extract_nb_features
     from random_forest import extract_rf_features
     from svc1 import extract_svc1_features
     from svc2 import extract_svc2_features
     import subprocess
 
+    paths = ''
+    with open(file_path, 'r') as f:
+        paths = f.readline().split(' ')
+
+    create_dir_if_not_exists(save_dir + '/knn_cells/')
+
     subprocess.run([
-        'go', 'run', dirname + '/kNN.go', '-folder', data_dir,
-        '-sites', '100', '-instances', '90', '-open', '9000',
-        '-new_path', save_dir + '/knn_cells/'])
+        'go', 'run', dirname + '/kNN.go', '-folder', data_dir + '/',
+        '-new_path', save_dir + '/knn_cells/', '-extension', extension, 'all-files']
+    )
 
-    extract_features(extract_nb_features, save_dir + '/nb_cells', data_dir=data_dir, extension=".cell", model_name="naive bayes")
-    extract_features(extract_rf_features, save_dir + '/rf_cells', data_dir=data_dir, extension=".cell", model_name="random forest")
-    extract_features(extract_svc1_features, save_dir + '/svc1_cells', data_dir=data_dir, extension=".cell", model_name="svc1")
-    extract_features(extract_svc2_features, save_dir + '/svc2_cells', data_dir=data_dir, extension=".cell", model_name="svc2")
 
+    extract_features_from_files(extract_nb_features, paths, save_dir + '/nb_cells', extension=extension, model_name="naive bayes")
+    extract_features_from_files(extract_rf_features, paths, save_dir + '/rf_cells', extension=extension, model_name="random forest")
+    extract_features_from_files(extract_svc1_features, paths, save_dir + '/svc1_cells', extension=extension, model_name="svc1")
+    extract_features_from_files(extract_svc2_features, paths, save_dir + '/svc2_cells', extension=extension, model_name="svc2")
+
+    stdout.write("Finished extracting features\n")
+
+def extract_all_features(save_dir, data_dir=DATA_DIR, extension=".cell"):
+    from naive_bayes import extract_nb_features
+    from random_forest import extract_rf_features
+    from svc1 import extract_svc1_features
+    from svc2 import extract_svc2_features
+    import subprocess
+
+    create_dir_if_not_exists(save_dir + '/knn_cells/')
+    subprocess.run([
+        'go', 'run', dirname + '/kNN.go', '-folder', data_dir + '/',
+        '-new_path', save_dir + '/knn_cells/', '-extension', extension]
+    )
+
+    extract_features(extract_nb_features, save_dir + '/nb_cells', data_dir=data_dir, extension=extension, model_name="naive bayes")
+    extract_features(extract_rf_features, save_dir + '/rf_cells', data_dir=data_dir, extension=extension, model_name="random forest")
+    extract_features(extract_svc1_features, save_dir + '/svc1_cells', data_dir=data_dir, extension=extension, model_name="svc1")
+    extract_features(extract_svc2_features, save_dir + '/svc2_cells', data_dir=data_dir, extension=extension, model_name="svc2")
 
     stdout.write("Finished extracting features\n")
 
 if __name__ == '__main__':
-    extract_all_features(DATA_DIR + '/..', DATA_DIR)
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Extracts the features from the cells")
+
+    parser.add_argument('--all_files', action='store_true', help="Whether to process all files or just the ones in `../data/X_test` (default false)")
+    parser.add_argument('--extension', metavar='', help="Extension of the cell files", default=".cell")
+
+    args = parser.parse_args()
+    if args.all_files:
+        extract_all_features(DATA_DIR + '/..', DATA_DIR, extension=args.extension)
+    else:
+        extract_partial_features(DATA_DIR + '/../X_test', DATA_DIR + '/..', DATA_DIR, extension=args.extension)
--- feature_extraction/kNN.go
+++ feature_extraction/kNN.go
@@ -1,239 +1,262 @@
 package main
 
 import (
-	"bufio"
-	"flag"
-	"io/ioutil"
-	"log"
-	"os"
-	"path"
-	"runtime"
-	"strconv"
-	"strings"
-	"sync"
+  "bufio"
+  "flag"
+  "io/ioutil"
+  "log"
+  "os"
+  "path"
+  "runtime"
+  "strconv"
+  "strings"
+  "sync"
+  "path/filepath"
 )
 
 // FeatureDelimiter is the delimiter in the output between features
 const FeatureDelimiter = " "
 
 func extract(times []float64, sizes []int) (features string, err error) {
-	// transmission size features
-	count := 0
-	for _, s := range sizes {
-		if s > 0 {
-			count++
-		}
-	}
-	features = strconv.Itoa(len(times))
-	features += FeatureDelimiter + strconv.Itoa(count)
-	features += FeatureDelimiter + strconv.Itoa(len(times)-count)
-	features += FeatureDelimiter + strconv.FormatFloat((times[len(times)-1]-times[0]), 'f', -1, 64)
+  // transmission size features
+  count := 0
+  for _, s := range sizes {
+    if s > 0 {
+      count++
+    }
+  }
+  features = strconv.Itoa(len(times))
+  features += FeatureDelimiter + strconv.Itoa(count)
+  features += FeatureDelimiter + strconv.Itoa(len(times)-count)
+  features += FeatureDelimiter + strconv.FormatFloat((times[len(times)-1]-times[0]), 'f', -1, 64)
 
-	// unique packet lengts
-	for i := -1500; i < 1501; i++ {
-		in := false
-		for _, s := range sizes {
-			if s == i {
-				in = true
-				break
-			}
-		}
-		if in {
-			features += FeatureDelimiter + strconv.Itoa(1)
-		} else {
-			features += FeatureDelimiter + strconv.Itoa(0)
-		}
-	}
+  // unique packet lengts
+  for i := -1500; i < 1501; i++ {
+    in := false
+    for _, s := range sizes {
+      if s == i {
+        in = true
+        break
+      }
+    }
+    if in {
+      features += FeatureDelimiter + strconv.Itoa(1)
+    } else {
+      features += FeatureDelimiter + strconv.Itoa(0)
+    }
+  }
 
-	// transpositions (similar to good distance scheme)
-	count = 0
-	for i := 0; i < len(sizes); i++ {
-		if sizes[i] > 0 {
-			count++
-			features += FeatureDelimiter + strconv.Itoa(i)
-		}
+  // transpositions (similar to good distance scheme)
+  count = 0
+  for i := 0; i < len(sizes); i++ {
+    if sizes[i] > 0 {
+      count++
+      features += FeatureDelimiter + strconv.Itoa(i)
+    }
 
-		if count == 300 {
-			break
-		}
-	}
-	for i := count; i < 300; i++ {
-		features += FeatureDelimiter + "'X'"
-	}
+    if count == 300 {
+      break
+    }
+  }
+  for i := count; i < 300; i++ {
+    features += FeatureDelimiter + "'X'"
+  }
 
-	count = 0
-	prevloc := 0
-	for i := 0; i < len(sizes); i++ {
-		if sizes[i] > 0 {
-			count++
-			features += FeatureDelimiter + strconv.Itoa(i-prevloc)
-			prevloc = i
-		}
-		if count == 300 {
-			break
-		}
-	}
-	for i := count; i < 300; i++ {
-		features += FeatureDelimiter + "'X'"
-	}
+  count = 0
+  prevloc := 0
+  for i := 0; i < len(sizes); i++ {
+    if sizes[i] > 0 {
+      count++
+      features += FeatureDelimiter + strconv.Itoa(i-prevloc)
+      prevloc = i
+    }
+    if count == 300 {
+      break
+    }
+  }
+  for i := count; i < 300; i++ {
+    features += FeatureDelimiter + "'X'"
+  }
 
-	// packet distributions (where are the outgoing packets concentrated)
-	// TODO: missing count = 0 reset here, but porting bug for now
-	// count = 0
-	for i := 0; i < len(sizes) && i < 3000; i++ {
-		if i%30 != 29 {
-			if sizes[i] > 0 {
-				count++
-			}
-		} else {
-			features += FeatureDelimiter + strconv.Itoa(count)
-			count = 0
-		}
-	}
-	for i := len(sizes) / 30; i < 100; i++ {
-		features += FeatureDelimiter + strconv.Itoa(0)
-	}
+  // packet distributions (where are the outgoing packets concentrated)
+  // TODO: missing count = 0 reset here, but porting bug for now
+  // count = 0
+  for i := 0; i < len(sizes) && i < 3000; i++ {
+    if i%30 != 29 {
+      if sizes[i] > 0 {
+        count++
+      }
+    } else {
+      features += FeatureDelimiter + strconv.Itoa(count)
+      count = 0
+    }
+  }
+  for i := len(sizes) / 30; i < 100; i++ {
+    features += FeatureDelimiter + strconv.Itoa(0)
+  }
 
-	// Bursts
-	curburst := 0
-	stopped := false
-	var bursts []int
-	for i := 0; i < len(sizes); i++ {
-		if sizes[i] < 0 {
-			stopped = false
-			curburst -= sizes[i]
-		}
-		if sizes[i] > 0 && !stopped {
-			stopped = true
-		}
-		if sizes[i] > 0 && stopped {
-			stopped = false
-			bursts = append(bursts, curburst)
-		}
-	}
-	max := -1
-	sum := 0
-	for i := 0; i < len(bursts); i++ {
-		sum += bursts[i]
-		if bursts[i] > max {
-			max = bursts[i]
-		}
-	}
-	features += FeatureDelimiter + strconv.Itoa(max)
-	features += FeatureDelimiter + strconv.Itoa(sum/len(bursts))
-	features += FeatureDelimiter + strconv.Itoa(len(bursts))
+  // Bursts
+  curburst := 0
+  stopped := false
+  var bursts []int
+  for i := 0; i < len(sizes); i++ {
+    if sizes[i] < 0 {
+      stopped = false
+      curburst -= sizes[i]
+    }
+    if sizes[i] > 0 && !stopped {
+      stopped = true
+    }
+    if sizes[i] > 0 && stopped {
+      stopped = false
+      bursts = append(bursts, curburst)
+    }
+  }
+  max := -1
+  sum := 0
+  for i := 0; i < len(bursts); i++ {
+    sum += bursts[i]
+    if bursts[i] > max {
+      max = bursts[i]
+    }
+  }
+  features += FeatureDelimiter + strconv.Itoa(max)
+  features += FeatureDelimiter + strconv.Itoa(sum/len(bursts))
+  features += FeatureDelimiter + strconv.Itoa(len(bursts))
 
-	counts := make([]int, 3)
-	for i := 0; i < len(bursts); i++ {
-		if bursts[i] > 5 {
-			counts[0]++
-		}
-		if bursts[i] > 10 {
-			counts[1]++
-		}
-		if bursts[i] > 15 {
-			counts[2]++
-		}
-	}
-	features += FeatureDelimiter + strconv.Itoa(counts[0])
-	features += FeatureDelimiter + strconv.Itoa(counts[1])
-	features += FeatureDelimiter + strconv.Itoa(counts[2])
+  counts := make([]int, 3)
+  for i := 0; i < len(bursts); i++ {
+    if bursts[i] > 5 {
+      counts[0]++
+    }
+    if bursts[i] > 10 {
+      counts[1]++
+    }
+    if bursts[i] > 15 {
+      counts[2]++
+    }
+  }
+  features += FeatureDelimiter + strconv.Itoa(counts[0])
+  features += FeatureDelimiter + strconv.Itoa(counts[1])
+  features += FeatureDelimiter + strconv.Itoa(counts[2])
 
-	for i := 0; i < 5; i++ {
-		if len(bursts) > i {
-			features += FeatureDelimiter + strconv.Itoa(bursts[i])
-		} else {
-			features += FeatureDelimiter + "'X'"
-		}
-	}
+  for i := 0; i < 5; i++ {
+    if len(bursts) > i {
+      features += FeatureDelimiter + strconv.Itoa(bursts[i])
+    } else {
+      features += FeatureDelimiter + "'X'"
+    }
+  }
 
-	for i := 0; i < 20; i++ {
-		if len(sizes) > i {
-			features += FeatureDelimiter + strconv.Itoa(sizes[i]+1500)
-		} else {
-			features += FeatureDelimiter + "'X'"
-		}
-	}
+  for i := 0; i < 20; i++ {
+    if len(sizes) > i {
+      features += FeatureDelimiter + strconv.Itoa(sizes[i]+1500)
+    } else {
+      features += FeatureDelimiter + "'X'"
+    }
+  }
 
-	return
+  return
 }
 
 func parse(filename, suffix string, new_path string) {
-	file, err := os.Open(filename + ".cell")
-	if err != nil {
-		log.Fatalf("failed to read file %s, got error %s", filename, err)
-	}
-	defer file.Close()
-	scanner := bufio.NewScanner(file)
+  file, err := os.Open(filename)
+  if err != nil {
+    log.Fatalf("failed to read file %s, got error %s", filename, err)
+  }
+  defer file.Close()
+  scanner := bufio.NewScanner(file)
 
-	var times []float64
-	var sizes []int
-	for scanner.Scan() {
-		items := strings.Split(scanner.Text(), "\t")
-		if len(items) != 2 {
-			log.Fatalf("expected 2 items in line for filename %s, got %d", filename, len(items))
-		}
+  var times []float64
+  var sizes []int
+  for scanner.Scan() {
+    items := strings.Split(scanner.Text(), "\t")
+    if len(items) != 2 {
+      log.Fatalf("expected 2 items in line for filename %s, got %d", filename, len(items))
+    }
 
-		t, er := strconv.ParseFloat(items[0], 64)
-		if er != nil {
-			log.Fatalf("failed to parse time for filename %s, %s", filename, er)
-		}
-		times = append(times, t)
+    t, er := strconv.ParseFloat(items[0], 64)
+    if er != nil {
+      log.Fatalf("failed to parse time for filename %s, %s", filename, er)
+    }
+    times = append(times, t)
 
-		s, er := strconv.ParseInt(items[1], 10, 64)
-		if er != nil {
-			log.Fatalf("failed to parse size for filename %s, %s", filename, er)
-		}
-		sizes = append(sizes, int(s))
-	}
+    s, er := strconv.ParseInt(items[1], 10, 64)
+    if er != nil {
+      log.Fatalf("failed to parse size for filename %s, %s", filename, er)
+    }
+    sizes = append(sizes, int(s))
+  }
 
-	features, err := extract(times, sizes)
-	if err != nil {
-		log.Fatalf("failed to extract features for filename %s, %s", filename, err)
-	}
-	err = ioutil.WriteFile(new_path + path.Base(filename) + suffix, []byte(features+FeatureDelimiter), 0666)
-	if err != nil {
-		log.Fatalf("failed to write features file for filename %s, %s", filename, err)
-	}
+  features, err := extract(times, sizes)
+  if err != nil {
+    log.Fatalf("failed to extract features for filename %s, %s", filename, err)
+  }
+  err = ioutil.WriteFile(new_path + path.Base(filename) + suffix, []byte(features+FeatureDelimiter), 0666)
+  if err != nil {
+    log.Fatalf("failed to write features file for filename %s, %s", filename, err)
+  }
 }
 
 func main() {
-	folder := flag.String("folder", "../data/cells/", "folder with cell traces")
-	sites := flag.Int("sites", 0, "number of sites")
-	open := flag.Int("open", 0, "number of open-world sites")
-	instances := flag.Int("instances", 0, "number of instances")
-	suffix := flag.String("suffix", ".cellf", "the suffix for the resulting files with parsed features")
-	new_path := flag.String("new_path", "../data/knn_cells/", "The the path to the folder where the features will be stored")
-	flag.Parse()
+  folder := flag.String("folder", "../data/cells/", "folder with cell traces")
+  suffix := flag.String("suffix", "f", "the suffix for the resulting files with parsed features")
+  extension := flag.String("extension", ".cell", "the extension for the cell files")
+  new_path := flag.String("new_path", "../data/knn_cells/", "The the path to the folder where the features will be stored")
+  all_files := flag.Bool("all_files", false, "Whether to include all files or just the ones in `../data/X_test`")
+  flag.Parse()
 
-	// workers
-	wg := new(sync.WaitGroup)
-	work := make(chan string)
-	for i := 0; i < runtime.NumCPU(); i++ {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			for filename := range work {
-				parse(filename, *suffix, *new_path)
-			}
-		}()
-	}
+  // workers
+  wg := new(sync.WaitGroup)
+  work := make(chan string)
+  for i := 0; i < runtime.NumCPU(); i++ {
+    wg.Add(1)
+    go func() {
+      defer wg.Done()
+      for filename := range work {
+        parse(filename, *suffix, *new_path)
+      }
+    }()
+  }
 
-	log.Printf("starting parsing...")
-	// closed world with specified number of instances
-	for site := 0; site < *sites; site++ {
-		for instance := 0; instance < *instances; instance++ {
-			work <- path.Join(*folder, strconv.Itoa(site)+"-"+strconv.Itoa(instance))
-		}
-	}
-	// open world, only one instance per site
-	for site := 0; site < *open; site++ {
-		work <- path.Join(*folder, strconv.Itoa(site))
-	}
+  log.Printf("Started parsing...")
 
-	close(work)
-	wg.Wait()
+  if(*all_files) {
+    files, err := ioutil.ReadDir(*folder)
+    if err != nil {
+      log.Fatal(err)
+    }
 
-	log.Printf("done parsing (%d sites, %d instances, %d open world, folder \"%s\", suffix \"%s\")",
-		*sites, *instances, *open, *folder, *suffix)
+    for _, file := range files {
+      currExtension := filepath.Ext(file.Name())
+      if(currExtension == *extension) {
+        work <- path.Join(*folder, file.Name())
+      }
+    }
+
+  } else {
+    file, err := os.Open(*folder + "../X_test")
+    if err != nil {
+        log.Fatal(err)
+    } else {
+      defer file.Close()
+
+      reader := bufio.NewReader(file)
+      fileContent, err := reader.ReadString('\n')
+
+      if err != nil {
+        log.Printf("Finished reading file")
+      }
+
+      s := strings.Split(fileContent, " ")
+      for _, path := range s {
+        work <- path
+      }
+    }
+  }
+
+  close(work)
+  wg.Wait()
+
+  log.Printf("Done parsing")
 }
